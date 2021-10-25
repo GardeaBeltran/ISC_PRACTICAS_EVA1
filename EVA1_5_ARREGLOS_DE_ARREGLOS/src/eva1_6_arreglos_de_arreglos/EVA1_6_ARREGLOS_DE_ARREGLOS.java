aa4826508e118fd4790fe7293943eefed3660473
GardeaBeltran <93112421+GardeaBeltran@users.noreply.github.com>
2021-10-24 23:09:44 -0600

    Add files via upload

---
+++ EVA1_5_ARREGLOS_DE_ARREGLOS/src/eva1_6_arreglos_de_arreglos/EVA1_6_ARREGLOS_DE_ARREGLOS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_6_arreglos_de_arreglos;
+
+/**
+ *
+ * @author Ximena Hernandez
+ */
+public class EVA1_6_ARREGLOS_DE_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
