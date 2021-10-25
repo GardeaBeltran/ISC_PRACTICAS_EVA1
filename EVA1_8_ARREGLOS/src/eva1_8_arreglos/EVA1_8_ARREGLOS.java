eac9b6653137e47f6b496d879df8721bdb0a65f5
GardeaBeltran <93112421+GardeaBeltran@users.noreply.github.com>
2021-10-24 23:10:57 -0600

    Add files via upload

---
+++ EVA1_8_ARREGLOS/src/eva1_8_arreglos/EVA1_8_ARREGLOS.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_8_arreglos;
+
+/**
+ *
+ * @author Ximena Hernandez
+ */
+public class EVA1_8_ARREGLOS {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] aiMiarreglo; //declaracion el arreglo todavia n existe
+        //arreglos son objetos
+        aiMiarreglo = new int[10]; //asignando memoria para 10 enteros
+        
+        int [] aiOtroarreglo = new int [50]; //declaramos y creamos el arreglo
+        
+        double [] adArre1, adArre2, adArre3;//tres arreglos de tipo double
+        
+        double adOtroArre[], dvariable, dOtraVar; // solo un arreglo y dos variables tipo double
+        
+       
+    }
+    
+}
