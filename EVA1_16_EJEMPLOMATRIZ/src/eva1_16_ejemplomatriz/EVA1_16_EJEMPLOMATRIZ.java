aa4323a0f58f4307e578bed1cc94692b475c0b18
GardeaBeltran <93112421+GardeaBeltran@users.noreply.github.com>
2021-10-24 23:13:12 -0600

    Add files via upload

---
+++ EVA1_16_EJEMPLOMATRIZ/src/eva1_16_ejemplomatriz/EVA1_16_EJEMPLOMATRIZ.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_16_ejemplomatriz;
+
+/**
+ *
+ * @author Ximena Hernandez
+ */
+public class EVA1_16_EJEMPLOMATRIZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] aiMatriz = new int[3][];
+        //FILAS
+        aiMatriz[0] = new int [2]; //fila cero son dos columnas
+        aiMatriz[1] = new int [10]; // fila uno son diez columnas
+        aiMatriz[2] = new int [5]; //filas dos son cincos columnas
+      //¿Cuantos enteros puedo almacdenar?
+      //tenemos 17 enteros
+    }
+    
+} 
