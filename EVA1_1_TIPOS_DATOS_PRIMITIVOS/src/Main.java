bdce857c5ba2fc67b617571a96903b2c49656025
GardeaBeltran <93112421+GardeaBeltran@users.noreply.github.com>
2021-10-24 23:05:04 -0600

    Add files via upload

---
+++ EVA1_1_TIPOS_DATOS_PRIMITIVOS/src/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ximena Hernandez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int iVal = Integer.MAX_VALUE;
+        System.out.println("Valor mas grande " + iVal);
+        iVal +=1;//overflow--> stackoverflow
+        System.out.println("Valor mas grande "+ iVal);
+        iVal= Integer.MIN_VALUE;
+        System.out.println("Valor mas pequeño " + iVal);
+    }
+    
+}
